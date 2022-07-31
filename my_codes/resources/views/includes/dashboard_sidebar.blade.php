c6d110039e580618703ae070c4d5e0b893d868b6
afattahi1387 <afattahi1387@gmail.com>
2022-07-31 20:20:35 +0430

    added publisher page link to admins dashboard sidebar.

--- my_codes/resources/views/includes/dashboard_sidebar.blade.php
+++ my_codes/resources/views/includes/dashboard_sidebar.blade.php
@@ -7,6 +7,10 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     داشبورد
                 </a>
+                <a class="nav-link" href="{{ route('publishers.page') }}">
+                    <div class="sb-nav-link-icon"><i class="fa fa-newspaper" aria-hidden="true"></i></div>
+                    انتشارات
+                </a>
             </div>
         </div>
     </nav>
