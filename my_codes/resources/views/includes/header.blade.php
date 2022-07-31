3fe6904cb1523f848ea760368f685b5deda09755
afattahi1387 <afattahi1387@gmail.com>
2022-07-31 06:44:45 +0430

    added dashboard link to top menu in home page.

--- my_codes/resources/views/includes/header.blade.php
+++ my_codes/resources/views/includes/header.blade.php
@@ -5,6 +5,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @if(Auth::check())
+                    <li class="nav-item"><a class="nav-link" href="{{ route('dashboard') }}">داشبورد</a></li>
                     <li class="nav-item"><a class="nav-link" onclick="event.preventDefault(); document.getElementById('logout_form').submit();" href="{{ route('logout') }}">خروج</a></li>
                     <form action="{{ route('logout') }}" id="logout_form" method="POST">
                         {{ csrf_field() }}
