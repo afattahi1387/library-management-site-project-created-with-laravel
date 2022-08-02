2008166a57a709f03fe28d15a2796bf8eedc2c76
afattahi1387 <afattahi1387@gmail.com>
2022-08-02 07:50:26 +0430

    added add book page to project.

---
+++ my_codes/resources/views/dashboard/add_book.blade.php
@@ -0,0 +1,89 @@
+@extends('includes.dashboard_html_structure')
+@section('icon', 'add.jpg')
+@section('title', 'افزودن کتاب')
+@section('content')
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">افزودن کتاب</h1><br>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-plus"></i>
+                        افزودن کتاب
+                    </div>
+                    <div class="card-body" style="direction: rtl;">
+                        <form action="{{ route('book.create') }}" method="POST">
+                            {{ csrf_field() }}
+                            <input type="text" name="name" placeholder="نام کتاب" class="form-control @if($errors->has('name')) is-invalid @endif">
+                            @if($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span><br>
+                            @endif
+                            <br>
+                            <textarea name="short_description" id="short_description" placeholder="توضیحات کوتاه" class="form-control" rows="15"></textarea>
+                            @if($errors->has('short_description'))
+                                <span class="text-danger">{{ $errors->first('short_description') }}</span><br>
+                            @endif
+                            <br>
+                            <textarea name="long_description" id="long_description" placeholder="توضیحات بلند" class="form-control" rows="15"></textarea>
+                            @if($errors->has('long_description'))
+                                <span class="text-danger">{{ $errors->first('long_description') }}</span><br>
+                            @endif
+                            <br>
+                            <label for="category_id">دسته بندی:</label>
+                            <select name="category_id" id="category_id" class="form-control">
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                @endforeach
+                            </select>
+                            <br>
+                            <label for="publisher_id">انتشارات:</label>
+                            <select name="publisher_id" id="publisher_id" class="form-control">
+                                @foreach($publishers as $publisher)
+                                    <option value="{{ $publisher->id }}">{{ $publisher->publisher_name }}</option>
+                                @endforeach
+                            </select>
+                            <br>
+                            <input type="submit" value="افزودن" class="btn btn-success">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
+
+    <script>
+      tinymce.init({
+        selector: '#short_description',
+        'directionality': 'rtl',
+        plugins: [
+
+          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+
+          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+
+          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+
+        ],
+        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+
+          'alignleft aligncenter alignright alignjustify | ' +
+
+          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+      });
+
+      tinymce.init({
+        selector: '#long_description',
+        'directionality': 'rtl',
+        plugins: [
+          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+        ],
+        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+          'alignleft aligncenter alignright alignjustify | ' +
+          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+      });
+    </script>
+@endsection
