5f53338ef2a26b95ba8ee19a99dc7810e3f17976
afattahi1387 <afattahi1387@gmail.com>
2022-08-07 08:43:09 +0430

    added show error.

--- my_codes/resources/views/main_views/books_for_category.blade.php
+++ my_codes/resources/views/main_views/books_for_category.blade.php
@@ -12,18 +12,22 @@
             <div class="col-lg-8">
                 <!-- Nested row for non-featured blog posts-->
                 <!-- Blog post-->
-                @foreach($books as $book)
-                    <div class="card mb-4">
-                        <a href="{{ route('single.book', ['book' => $book->id]) }}"><img class="card-img-top" src="{{ asset('/images/books_images/' . $book->image) }}" alt="تصویر به نمایش در نیامد." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">دسته بندی: {{ $book->category->category_name }}</div><br>
-                            <div class="small text-muted">انتشارات: {{ $book->publisher->publisher_name }}</div><br>
-                            <h2 class="card-title h4">{{ $book->name }}</h2>
-                            <p class="card-text">{!! $book->short_description !!}</p>
-                            <a class="btn btn-primary" href="{{ route('single.book', ['book' => $book->id]) }}">مشاهده اطلاعات کتاب →</a>
+                @if($books->count() < 1)
+                    <div class="alert-danger" style="height: 30px; padding: 3px; border: none; border-radius: 5px;">کتابی یافت نشد!</div>
+                @else
+                    @foreach($books as $book)
+                        <div class="card mb-4">
+                            <a href="{{ route('single.book', ['book' => $book->id]) }}"><img class="card-img-top" src="{{ asset('/images/books_images/' . $book->image) }}" alt="تصویر به نمایش در نیامد." /></a>
+                            <div class="card-body">
+                                <div class="small text-muted">دسته بندی: {{ $book->category->category_name }}</div><br>
+                                <div class="small text-muted">انتشارات: {{ $book->publisher->publisher_name }}</div><br>
+                                <h2 class="card-title h4">{{ $book->name }}</h2>
+                                <p class="card-text">{!! $book->short_description !!}</p>
+                                <a class="btn btn-primary" href="{{ route('single.book', ['book' => $book->id]) }}">مشاهده اطلاعات کتاب →</a>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                @endif
                 {{ $books->links() }}
             </div>
             @include('includes.main_pages_sidebar')
