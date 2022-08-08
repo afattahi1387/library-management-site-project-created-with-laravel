84dbf1be7d0fd2f8dbadec5acdd2684d27b7fa5c
afattahi1387 <afattahi1387@gmail.com>
2022-08-08 19:03:28 +0430

    added type column to users table.

--- my_codes/database/migrations/2014_10_12_000000_create_users_table.php
+++ my_codes/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('type', 255)->default('user');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
