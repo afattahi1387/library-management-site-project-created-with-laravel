170045d7fe9be59860b9afecc52e81b401780a01
afattahi1387 <afattahi1387@gmail.com>
2022-08-09 08:39:43 +0430

    added trusts table to database.

---
+++ my_codes/app/Trust.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trust extends Model
+{
+    protected $fillable = ['book_id', 'user_id', 'trusted_at'];
+
+    public function book() {
+        return $this->hasMany(Book::class);
+    }
+}
---
+++ my_codes/database/migrations/2022_08_09_030425_create_trusts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrustsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trusts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('trusted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trusts');
+    }
+}
