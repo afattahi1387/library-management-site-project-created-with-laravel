431c809d8ee3bb771c8833d2e8fbd60f3181adf2
afattahi1387 <afattahi1387@gmail.com>
2022-08-13 19:26:00 +0430

    changed books_page file.

--- my_codes/resources/views/dashboard/books_page.blade.php
+++ my_codes/resources/views/dashboard/books_page.blade.php
@@ -29,12 +29,16 @@
                                 @foreach ($books as $book)
                                     <tr>
                                         <td>@php echo ++$booksCounter; @endphp</td>
-                                        <td>{{ $book->name }}<br><span style="color: red;">({{ $book->quantity }} تا)</span></td>
+                                        <td>{{ $book->name }}<br><span style="color: red;">({{ $book->quantity }} عدد)</span></td>
                                         <td>
                                             <img src="/images/books_images/{{ $book->image }}" style="width: 150px; height: 50px; border-radius: 5px;" alt="تصویری به نمایش در نیامد.">
                                         </td>
                                         <td>{{ $book->category->category_name }}</td>
-                                        <td>{{ $book->publisher->publisher_name }}</td>
+                                        <td>
+                                            {{ $book->publisher->publisher_name }}
+                                            <br>
+                                            <span style="color: red;">()</span>
+                                        </td>
                                         <td>
                                             <div class="d-flex">
                                                 <a href="{{ route('single.book', ['book' => $book->id]) }}" target="_blank" style="margin-right: 2px;" class="btn btn-primary">مشاهده</a>
