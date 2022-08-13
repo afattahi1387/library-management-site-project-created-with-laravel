c41859b4612483e4084d5378ebea2c03d7d8e515
afattahi1387 <afattahi1387@gmail.com>
2022-08-13 19:23:02 +0430

    added status method and change fillable in Trust Model.

--- my_codes/app/Trust.php
+++ my_codes/app/Trust.php
@@ -6,9 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trust extends Model
 {
-    protected $fillable = ['book_id', 'user_id', 'trusted_at'];
+    protected $fillable = ['book_id', 'extended', 'user_id', 'trusted_at'];
 
     public function book() {
         return $this->belongsTo(Book::class);
+    }
+
+    function status() {
+        $time1 = 3600 * 24 * 14;
+        $time2 = floor(time() - $this->trusted_at - $time1);
+        if($time2 < 0) {
+            return ['good', ''];
+        } else {
+            $day = 3600 * 24;
+            $penalty_time = floor($time2 / $day);
+            $penalty_price = $penalty_time * env('PENALTY_PRICE');
+            return ['penalty', $penalty_price];
+        }
     }
 }
