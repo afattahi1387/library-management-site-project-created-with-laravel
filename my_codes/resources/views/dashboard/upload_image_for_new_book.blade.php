d6328f4756a048fed7afbe6fee482e14beaa9a88
afattahi1387 <afattahi1387@gmail.com>
2022-08-14 06:32:13 +0430

    deleted tinymce settings from this file.

--- my_codes/resources/views/dashboard/upload_image_for_new_book.blade.php
+++ my_codes/resources/views/dashboard/upload_image_for_new_book.blade.php
@@ -29,40 +29,4 @@
             </div>
         </main>
     </div>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-
-
-    <script>
-      tinymce.init({
-        selector: '#short_description',
-        'directionality': 'rtl',
-        plugins: [
-
-          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-
-          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-
-          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-
-        ],
-        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
-
-          'alignleft aligncenter alignright alignjustify | ' +
-
-          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
-      });
-
-      tinymce.init({
-        selector: '#long_description',
-        'directionality': 'rtl',
-        plugins: [
-          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-        ],
-        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
-          'alignleft aligncenter alignright alignjustify | ' +
-          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
-      });
-    </script>
 @endsection
