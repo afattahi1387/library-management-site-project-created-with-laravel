5d5acffc6849fcde86dfe888c676a4c50979faf9
afattahi1387 <afattahi1387@gmail.com>
2022-08-15 16:35:47 +0430

    edited admin panel writers.

--- my_codes/resources/views/dashboard/admin_panel_writers.blade.php
+++ my_codes/resources/views/dashboard/admin_panel_writers.blade.php
@@ -27,22 +27,24 @@
                                     $writersCounter = 0;
                                 @endphp
                                 @foreach ($writers as $writer)
-                                    <td>@php echo ++$writersCounter; @endphp</td>
-                                    <td>{{ $writer->name }}</td>
-                                    <td>
-                                        <img src="@if(empty($writer->image)) {{ asset('images/writers_images/undefined_writer.png') }} @else {{ asset('images/writers_images/' . $writer->image) }} @endif" style="width: 150px; height: 50px; border-radius: 5px;" alt="تصویری به نمایش در نیامد.">
-                                    </td>
-                                    <td>
-                                        <div class="d-flex">
-                                            <a href="{{ route('single.writer', ['writer' => $writer->id]) }}" target="_blank" class="btn btn-primary" style="margin-right: 2px;">مشاهده مشخصات نویسنده</a>
-                                            <a href="{{ route('writer.edit', ['writer' => $writer->id]) }}" class="btn btn-warning" style="margin-right: 2px; color: white;">ویرایش</a>
-                                            <form action="{{ route('writer.delete', ['writer' => $writer->id]) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                <input type="hidden" name="_method" value="delete">
-                                                <button class="btn btn-danger" onclick="if(confirm('آیا از حذف این نویسنده مطمئـن هستید؟')){return true;}else{return false;}">حذف</button>
-                                            </form>
-                                        </div>
-                                    </td>
+                                    <tr>
+                                        <td>@php echo ++$writersCounter; @endphp</td>
+                                        <td>{{ $writer->name }}</td>
+                                        <td>
+                                            <img src="@if(empty($writer->image)) {{ asset('images/writers_images/undefined_writer.png') }} @else {{ asset('images/writers_images/' . $writer->image) }} @endif" style="width: 150px; height: 50px; border-radius: 5px;" alt="تصویری به نمایش در نیامد.">
+                                        </td>
+                                        <td>
+                                            <div class="d-flex">
+                                                <a href="{{ route('single.writer', ['writer' => $writer->id]) }}" target="_blank" class="btn btn-primary" style="margin-right: 2px;">مشاهده مشخصات نویسنده</a>
+                                                <a href="{{ route('writer.edit', ['writer' => $writer->id]) }}" class="btn btn-warning" style="margin-right: 2px; color: white;">ویرایش</a>
+                                                <form action="{{ route('writer.delete', ['writer' => $writer->id]) }}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="_method" value="delete">
+                                                    <button class="btn btn-danger" onclick="if(confirm('آیا از حذف این نویسنده مطمئـن هستید؟')){return true;}else{return false;}">حذف</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
