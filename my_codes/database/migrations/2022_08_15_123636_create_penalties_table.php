818c6e293b513deb7fa5021ae5beec4a4cdbb057
afattahi1387 <afattahi1387@gmail.com>
2022-08-16 07:01:49 +0430

    added book name column to penalties migration.

--- my_codes/database/migrations/2022_08_15_123636_create_penalties_table.php
+++ my_codes/database/migrations/2022_08_15_123636_create_penalties_table.php
@@ -17,6 +17,7 @@ class CreatePenaltiesTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('book_name', 255);
             $table->integer('penalty');
             $table->timestamps();
         });
