d4b8f6a0525b19d2abe801f349532e2d48c170d0
afattahi1387 <afattahi1387@gmail.com>
2022-08-18 07:52:21 +0430

    added show comments to single book page.

--- my_codes/resources/views/main_views/single_book.blade.php
+++ my_codes/resources/views/main_views/single_book.blade.php
@@ -68,39 +68,16 @@
                                     <textarea class="form-control" name="comment" rows="4" id="comment" placeholder="نظر شما"></textarea><br>
                                     <input type="submit" value="افزودن" class="btn btn-success">
                                 </form>
-                                <!-- Comment with nested comments-->
-                                <div class="d-flex mb-4">
-                                    <!-- Parent comment-->
-                                    <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                    <div class="ms-3">
-                                        <div class="fw-bold">Commenter Name</div>
-                                        If you're going to lead a space frontier, it has to be government; it'll never be private enterprise. Because the space frontier is dangerous, and it's expensive, and it has unquantified risks.
-                                        <!-- Child comment 1-->
-                                        <div class="d-flex mt-4">
-                                            <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                            <div class="ms-3">
-                                                <div class="fw-bold">Commenter Name</div>
-                                                And under those conditions, you cannot establish a capital-market evaluation of that enterprise. You can't get investors.
-                                            </div>
+                                <h4 id="comments">نظرات داده شده</h4><br>
+                                @foreach($book->comments as $comment)
+                                    <div class="d-flex">
+                                        <div class="flex-shrink-0"><img class="rounded-circle" style="width: 60px; height: 60px;" src="/images/users_images/@if(empty($comment->user_profile))undefined_user.png @else{{ $comment->user_profile }} @endif" alt="..." /></div>
+                                        <div class="ms-3">
+                                            <div class="fw-bold">{{ $comment->user_name }}</div>
+                                            {!! $comment->comment !!}
                                         </div>
-                                        <!-- Child comment 2-->
-                                        <div class="d-flex mt-4">
-                                            <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                            <div class="ms-3">
-                                                <div class="fw-bold">Commenter Name</div>
-                                                When you put money directly to a problem, it makes a good headline.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Single comment-->
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                    <div class="ms-3">
-                                        <div class="fw-bold">Commenter Name</div>
-                                        When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
-                                    </div>
-                                </div>
+                                    </div><br>
+                                @endforeach
                             </div>
                         </div>
                     </section>
