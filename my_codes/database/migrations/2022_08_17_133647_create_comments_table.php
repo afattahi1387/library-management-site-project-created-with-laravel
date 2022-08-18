d6b5d84826dac41ed4ae69041df54544562c6d3b
afattahi1387 <afattahi1387@gmail.com>
2022-08-18 07:26:41 +0430

    added comments table migration.

---
+++ my_codes/database/migrations/2022_08_17_133647_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_profile', 255)->nullable();
+            $table->string('user_name', 255);
+            $table->text('comment');
+            $table->integer('user_id')->default(0);
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
