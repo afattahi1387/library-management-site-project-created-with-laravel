3eeaef4103624763d478dcded22ebf2a94baff6b
afattahi1387 <afattahi1387@gmail.com>
2022-08-18 07:27:17 +0430

    added Comment Model and fillable array.

---
+++ my_codes/app/Comment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['user_profile', 'user_name', 'comment', 'user_id', 'book_id'];
+}
