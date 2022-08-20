6a5869788ab791b94df799652a5e8146ae4e6459
afattahi1387 <afattahi1387@gmail.com>
2022-08-20 19:32:42 +0430

    added book method to Comment model for get comment books.

--- my_codes/app/Comment.php
+++ my_codes/app/Comment.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['user_profile', 'user_name', 'comment', 'user_id', 'book_id'];
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
 }
