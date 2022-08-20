2d6effb1eee53a15ebc29656fc95542e536a02a8
afattahi1387 <afattahi1387@gmail.com>
2022-08-20 17:27:02 +0430

    added books method to Writer model.

--- my_codes/app/Writer.php
+++ my_codes/app/Writer.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Writer extends Model
 {
     protected $fillable = ['name', 'description', 'image'];
+
+    public function books() {
+        return $this->hasMany(Book::class);
+    }
 }
