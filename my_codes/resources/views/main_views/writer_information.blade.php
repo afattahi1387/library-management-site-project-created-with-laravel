ba484169fa1ebbc447c8d2b84e16ce7a6fda3ade
afattahi1387 <afattahi1387@gmail.com>
2022-08-23 15:07:13 +0430

    added writer information page link to single book page.

--- my_codes/resources/views/main_views/writer_information.blade.php
+++ my_codes/resources/views/main_views/writer_information.blade.php
@@ -12,5 +12,32 @@
         <span>{{ $writer->name }}</span>
         <hr>
     </div>
-    <div style="margin-right: 10px;">{!! $writer->description !!}</div>
+    <div style="margin-right: 10px;">{!! $writer->description !!}</div><br>
+    @if($writer->books->count() < 1)
+        <div class="alert alert-danger" role="alert">
+            کتابی یافت نشد!
+        </div>
+    @else
+            <table class="table table-bordered">
+                <tr>
+                    <th>ردیف</th>
+                    <th>تصویر</th>
+                    <th>نام کتاب</th>
+                </tr>
+                @php $booksCounter = 0; @endphp
+                @foreach($writer->books as $book)
+                    <tr>
+                        <td>@php echo ++$booksCounter; @endphp</td>
+                        <td>
+                            <img src="/images/books_images/{{ $book->image }}" alt="تصویری به نمایش در نیامد." style="width: 300px; border-radius: 5px;">
+                        </td>
+                        <td>
+                            {{ $book->name }}
+                            <br>
+                            <a href="{{ route('single.book', ['book' => $book->id]) }}" target="_blank" class="btn btn-primary">مشاهده</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+    @endif
 @endsection
