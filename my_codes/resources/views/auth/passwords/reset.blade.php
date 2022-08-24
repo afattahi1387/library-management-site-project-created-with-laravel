79b8227d5ab07b1b2a945a06764e2e9fd09cba5d
afattahi1387 <afattahi1387@gmail.com>
2022-08-24 23:15:40 +0430

    added reset password page to project.

---
+++ my_codes/resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,48 @@
+@extends('includes.authentication_html_structure')
+@section('icon', 'reset_password.png')
+@section('title', 'تغییر رمز عبور')
+@section('content')
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header"><h3 class="text-center font-weight-light my-4">تغییر رمز عبور</h3></div>
+                                <div class="card-body">
+                                    <div class="small mb-3 text-muted" style="direction: rtl;">لطفا رمز عبور جدید خود را وارد کنید.</div>
+                                    <form action="/password/reset" method="POST">
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="token" value="{{ $token }}">
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control @if($errors->has('email')) is-invalid @endif" name="email" id="email" type="email" placeholder="ایمیل" value="{{ $email }}" />
+                                            <label for="email">ایمیل</label>
+                                            @if($errors->has('email'))
+                                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                                            @endif
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" name="password" id="password" type="password" placeholder="رمز عبور جدید" />
+                                            <label for="email">رمز عبور جدید</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" name="password_confirmation" id="password_confirmation" type="password" placeholder="تکرار رمز عبور جدید" />
+                                            <label for="email">تکرار رمز عبور جدید</label>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            <input type="submit" value="ثبت" class="btn btn-success">
+                                        </div>
+                                    </form>
+                                </div>
+                                {{-- <div class="card-footer text-center py-3">
+                                    <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                                </div> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+@endsection
