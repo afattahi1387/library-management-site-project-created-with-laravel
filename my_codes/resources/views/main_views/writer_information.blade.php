d5b73285e1baa565cfcc9d5803cbb5d724050330
afattahi1387 <afattahi1387@gmail.com>
2022-09-13 07:53:53 +0430

    seted show follow button.

--- my_codes/resources/views/main_views/writer_information.blade.php
+++ my_codes/resources/views/main_views/writer_information.blade.php
@@ -12,7 +12,22 @@
         <span>{{ $writer->name }}</span>
         <hr>
     </div>
-    <div style="margin-right: 10px;">{!! $writer->description !!}</div><br>
+    <div style="margin-right: 10px;">{!! $writer->description !!}</div>
+	@if(auth()->check() && auth()->user()->type != 'admin' && !($writer->is_follower(auth()->user()->id)))
+		<br>
+		<a href="{{ route('follow.writer', ['writer' => $writer->id]) }}" class="btn btn-primary" style="margin-right: 10px;">دنبال کردن</a>
+	@elseif(auth()->check() && $writer->is_follower(auth()->user()->id))
+		<br>
+		<div class="alert alert-primary">شما دنبال کننده این نویسنده هستید.</div>
+	@elseif(auth()->check() && auth()->user()->type == 'admin')
+		<br>
+		<a href="{{ route('writer.edit', ['writer' => $writer->id]) }}" class="btn btn-warning" style="color: white; margin-right: 10px;">ویرایش نویسنده</a>
+	@elseif(!(auth()->check()))
+		<br>
+		<a href="#" class="btn btn-primary disabled" style="margin-right: 10px;">دنبال کردن</a>
+		<span class="text-danger">برای دنبال کردن این نویسنده، ابتدا باید وارد شوید.</span>
+	@endif
+	<br><br>
     @if($writer->books->count() < 1)
         <div class="alert alert-danger" role="alert">
             کتابی یافت نشد!
@@ -40,15 +55,6 @@
             @endforeach
         </table>
     @endif
-    @if(auth()->check() && auth()->user()->type != 'admin' && !($writer->is_follower(auth()->user()->id)))
-        <br>
-        <a href="{{ route('follow.writer', ['writer' => $writer->id]) }}" class="btn btn-primary" style="margin-right: 10px;">دنبال کردن</a>
-    @elseif($writer->is_follower(auth()->user()->id))
-        <br>
-        <div class="alert alert-primary">شما دنبال کننده این نویسنده هستید.</div>
-    @elseif(auth()->check() && auth()->user()->type == 'admin')
-        <br>
-        <a href="{{ route('writer.edit', ['writer' => $writer->id]) }}" class="btn btn-warning" style="color: white; margin-right: 10px;">ویرایش نویسنده</a>
-    @endif
+    
     <br><br>
 @endsection
